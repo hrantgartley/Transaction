a96d87ab3f6c7424ce47af2fb3bcc0d54a0fb9d0
Grant Hartley <89709057+hrantgartley@users.noreply.github.com>
2023-10-08 22:18:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from trans import *
 def main():
     first = input("Enter your full name: ")
     names = first.split()
-    customer = Customer(names[0], names[1], '256-349-8162')
+    customer = Customer(names[0], names[1], '256-555-5555')
     print(customer)
 
 
