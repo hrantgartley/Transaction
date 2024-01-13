8f6710f92da8e21dca8ad2fc32b6c47dd0840696
hrantgartley <doyouevenjump@gmail.com>
2024-01-13 00:09:37 -0600

    star import bad

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from trans import *
+from trans import Customer
+
 
 def main():
     first = input("Enter your full name: ")
     names = first.split()
-    customer = Customer(names[0], names[1], '256-555-5555')
+    customer = Customer(names[0], names[1], "256-555-5555")
     print(customer)
 
 
